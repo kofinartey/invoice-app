ebdc21f5369358f75239b23f03427256b956c71d
Kofi Nartey <eugeneahulu15@live.com>
2021-12-25 22:26:14 +0000

    fixed theme base url

--- client/src/redux/theme/themeAction.js
+++ client/src/redux/theme/themeAction.js
@@ -3,13 +3,16 @@ import { SWITCH_THEME } from "./themeTypes";
 export const switchTheme = () => async (dispatch) => {
   try {
     const token = JSON.parse(localStorage.getItem("userInfo")).token;
-    const response = await fetch("http://localhost:5000/api/users/theme", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        "x-auth-token": token,
-      },
-    });
+    const response = await fetch(
+      `${process.env.REACT_APP_BASE_URL}/users/theme`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": token,
+        },
+      }
+    );
     const data = await response.json();
     dispatch({ type: SWITCH_THEME, payload: data });
   } catch (error) {
